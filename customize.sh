b7bd484393a0dfd89e4fb72ea35ce690530695b6
JUANIMAN <juanimandarin9@gmail.com>
2024-07-17 18:13:01 -0600

    Improved module installation script

--- customize.sh
+++ customize.sh
@@ -3,56 +3,55 @@ SKIPUNZIP=1
 # Module version
 MODVER=$(grep_prop version $TMPDIR/module.prop)
 
-# System language
+# System information
 SYSLANG=$(getprop persist.sys.locale)
-
-# Device brand
 BRAND=$(getprop ro.product.brand)
-
-# SOC
 SOC=$(getprop ro.hardware)
+
+# RAM
+total_ram_kb=$(grep MemTotal /proc/meminfo | awk '{print $2}')
+total_ram_mb=$((total_ram_kb / 1024))
 
 # Function to replace a property in the system.prop file
 replace_property() {
   local property="$1"
   local value="$2"
+  local file="$3"
 
-  sed -i "s/$property=/$property=$value/" "$file.bak"
+  sed -i "s/$property=.*/$property=$value/" "$file"
 }
 
 # Function to check RAM size and set low_ram property
 set_low_ram_property() {
-  local total_ram_kb
-  local total_ram_mb
-  total_ram_kb="$(grep MemTotal /proc/meminfo | awk '{print $2}')"
-  total_ram_mb="$((total_ram_kb / 1024))"
+  local file="$1"
 
   if [ $total_ram_mb -lt 3072 ]; then
-    replace_property ro.config.low_ram true
+    replace_property ro.config.low_ram true "$file"
   else
-    replace_property ro.config.low_ram false
+    replace_property ro.config.low_ram false "$file"
   fi
 }
 
 # Function to set gfx.driver.0 property
 set_gfx_driver_property() {
+  local file="$1"
   local gfxgd="com.mediatek.$SOC.gamedriver"
-  replace_property ro.gfx.driver.0 "$gfxgd"
+
+  replace_property ro.gfx.driver.0 "$gfxgd" "$file"
 }
 
 install_module() {
-  unzip -o "$ZIPFILE" -x 'META-INF/*' 'LICENSE' -d $MODPATH >&2
+  unzip -o "$ZIPFILE" -x 'META-INF/*' 'LICENCE' -d $MODPATH >&2
 
-  local file="$MODPATH/system.prop"
-  cp "$file" "$file.bak"
+  local prop_file="$MODPATH/system.prop"
+  cp "$prop_file" "$prop_file.bak"
 
-  # Call helper functions
-  set_low_ram_property
-  set_gfx_driver_property
+  set_low_ram_property "$prop_file.bak"
+  set_gfx_driver_property "$prop_file.bak"
 
-  mv "$file.bak" "$file"
+  mv "$prop_file.bak" "$prop_file"
   set_perm_recursive $MODPATH 0 0 0755 0644
-  set_perm_recursive $MODPATH/system/bin 0 0 0775 0775
+  set_perm_recursive $MODPATH/system/bin 0 2000 0755 0755
 }
 
 print_banner() {
